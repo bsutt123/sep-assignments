d124a59613a0ee7ee7d447cb106b4e1c9bc52748
bsutt123 <bsutt123@gmail.com>
2017-09-25 11:52:15 -0400

    created mystack and myqueue

--- 01-data-structures/02-stacks-and-queues/mystack/mystack.rb
+++ 01-data-structures/02-stacks-and-queues/mystack/mystack.rb
@@ -1,17 +1,34 @@
 class MyStack
-  attr_accessor :top
+  attr_reader :top
 
   def initialize
     @stack = Array.new
-    self.top = nil
+    @top = nil
   end
 
   def push(item)
+    if empty?
+      @stack[0] = item
+    else
+      @stack[@stack.length] = item
+    end
+    @top = @stack[-1]
   end
 
   def pop
+    if !empty?
+      top_element = @stack[-1]
+      if @stack.length == 1
+        @stack = []
+      else
+        @stack = @stack[0...(@stack.length-1)]
+      end
+      @top = @stack[-1]
+      top_element
+    end
   end
 
   def empty?
+    @stack == []
   end
 end
