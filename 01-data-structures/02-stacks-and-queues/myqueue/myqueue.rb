9c265be74bc5cf5b51332c731c2201844b3568f1
bsutt123 <bsutt123@gmail.com>
2017-09-25 12:00:08 -0400

    refactored using delete_at

--- 01-data-structures/02-stacks-and-queues/myqueue/myqueue.rb
+++ 01-data-structures/02-stacks-and-queues/myqueue/myqueue.rb
@@ -1,18 +1,35 @@
 class MyQueue
-  attr_accessor :head
-  attr_accessor :tail
+  attr_reader :head
+  attr_reader :tail
 
   def initialize
     @queue = Array.new
     @head = @queue[0]
+    @tail = @queue[-1]
   end
 
   def enqueue(element)
+    if empty?
+      @queue[0] = element
+    else
+      @queue[@queue.length] = element
+    end
+    @head = @queue[0]
+    @tail = @queue[-1]
   end
 
   def dequeue
+    if !empty?
+      first_element = @queue[0]
+
+      @queue.delete_at(0)
+      @head = @queue[0]
+      @tail = @queue[-1]
+      return first_element
+    end
   end
 
   def empty?
+    @queue == []
   end
 end
